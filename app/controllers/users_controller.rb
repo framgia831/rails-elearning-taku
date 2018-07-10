ae9545a4b3d3df689adcfbbc8100efe2772f8d1c
Your Actual Name <Your Actual Email>
2018-07-10 14:45:58 +0800

    User settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Successfully updated!"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
