b868addb5a1196af4ccc89d775aead6805bec4a9
Your Actual Name <Your Actual Email>
2018-07-10 17:59:32 +0800

    Login/Logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  include SessionHelper
+
+  def require_login
+    unless logged_in?
+      flash[:danger] = "Plese log-in"
+      redirect_to root_url
+    end
+  end
 end
