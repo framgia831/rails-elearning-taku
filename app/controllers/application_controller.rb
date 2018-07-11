8275b3660c40f46f7bc307facb2c16c662643096
Your Actual Name <Your Actual Email>
2018-07-11 22:14:22 +0800

    User list(using faker and pagination)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@ class ApplicationController < ActionController::Base
 
   def require_login
     unless logged_in?
-      flash[:danger] = "Plese log-in"
+      flash[:danger] = "Plese log-in."
       redirect_to root_url
     end
+  end
+
+  def correct_user
+  	@user = User.find(params[:id])
+  	unless current_user?(@user)
+  		flash[:danger] = "Have no authority."
+  		redirect_to root_url
+  	end
   end
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -15,4 +15,8 @@ module SessionHelper
   def current_user
     @current_user = User.find_by(id: session[:user_id])
   end
+
+  def current_user?(user)
+    user == current_user
+  end
 end
