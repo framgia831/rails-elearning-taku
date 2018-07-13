09f24bd75ed3973979acc02c6d4af55c99bcbb64
Your Actual Name <Your Actual Email>
2018-07-13 16:46:39 +0800

    Follow/Unfollow function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,35 @@
 class User < ApplicationRecord
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save {email.downcase!}
 
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-before_save {email.downcase!}
+  validates :name, presence: true, length:{maximum: 50}
+  validates :email, presence: true, length:{maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness:{case_sensitive: false} 
+  has_secure_password
 
-validates :name, presence: true, length:{maximum: 50}
-validates :email, presence: true, length:{maximum: 255},
-          format: {with: VALID_EMAIL_REGEX},
-          uniqueness:{case_sensitive: false} 
-has_secure_password
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
 
+  has_many :following, through: :active_relationships,
+                      source: :followed
+  has_many :followers, through: :passive_relationships,
+                      source: :follower
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
 end
