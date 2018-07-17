f71fa28c609f36d2f75d717d99ef0c45286389cf
Your Actual Name <Your Actual Email>
2018-07-17 16:50:03 +0800

    Admin function for category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,17 @@ class ApplicationController < ActionController::Base
   end
 
   def correct_user
-  	@user = User.find(params[:id])
-  	unless current_user?(@user)
-  		flash[:danger] = "Have no authority."
-  		redirect_to root_url
-  	end
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      flash[:danger] = "Have no authority."
+      redirect_to root_url
+    end
+  end
+
+  def require_admin
+    unless logged_in? && current_user.admin?
+      flash[:danger] = "Have no authority."
+      redirect_to root_url
+    end
   end
 end
